5370439b1034a6d84ca7e294062d2517d77c8f28
Andres <andresshehadeh@hotmail.com>
2017-04-02 20:35:16 -0300

    Add files via upload

---
+++ anagramas.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void error(char mensaje[]){
+		printf("Ocurrio un error al %s\n",mensaje); """ esta wea es pa probar algo (:"""
+int anagramas(char **S,int n,char *str){
+}
+int main(){
+	FILE*archivo;
+	archivo=fopen("strings.txt","r");
+	int palabras,anagrama,i;
+	while(fscanf("%d",palabras)!="EOF");{
+		char *ldpalabras[palabras];
+		s=char malloc(palabras*201*sizeof(char *));
+		for(i=0;i<palabras;i++);{
+			char leido[201];fscanf("%s",leido);
+			s[i]=leido;
+			ldpalabras[i]=&s[i];
+		}
+		fscanf("%d",&anagrama);
+		FILE*salida;
+		salida=fopen("salida-1.txt","a");
+		for(i=0;i<anagrama;i++){
+			fscanf("%s",leido);
+			fprintf(salida,"%d\n",anagramas(ldpalabras,palabras,leido));
+		}
+		fclose(salida);
+		free(s);
+	}
+	
+
+	return 0;
+}
