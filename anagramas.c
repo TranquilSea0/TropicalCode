e23b28317c190adc0e583e5a6516582038d1bd0f
Andres <andresshehadeh@hotmail.com>
2017-04-04 13:37:41 -0300

    Update anagramas.c

--- anagramas.c
+++ anagramas.c
@@ -2,54 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-void sort(char* A, char* B){
+void sort(char* inputString, char* outputArray){
     int counterArray[256] ={0}, length, counter, index;
-    length = strlen(A);
+    length = strlen(inputString);
     for(counter = 0; counter < length; counter++){
         counterArray[inputString[counter]]++;
     }
     for(counter = 0, index = 0; counter < 256; counter++){
         if(counterArray[counter] != 0){
             while(counterArray[counter] > 0){
-                B[index++] = counter;
+                outputArray[index++] = counter;
                 counterArray[counter]--;
             }
         }
     }
-    B[index] = '\0';
+    outputArray[index] = '\0';
 }
 void error(char n[]){
 	printf("%s",n);
-	exit(1)
+	exit(1);
 }
 void cambio(char *palabra){
 	int i=0;
 	while(palabra[i] !="\0"){
-		if((97<=palabra[i]) && (palabra[i]<=122))
-			continue;
-		if else((65<=palabra[i]) && (palabra[i]<95))
-			palabra[i]+=32;
-		if else((palabra[i]==160) || (palabra[i]==181))
-			palabra[i]="a";
+		if((97<=palabra[i]) && (palabra[i]<=122)){
+			continue;}
+		else if((65<=palabra[i]) && (palabra[i]<95)){
+			palabra[i]+=32;}
 		i++;
 	}
 }
-char **array(FILE *texto,numero){
+char **array(FILE *texto,int numero){
     char **array_p,*tropical,copia[201];
-    int i,j;
+    int i;
     array_p=(char **)malloc(sizeof(char*)*numero);
     for(i=0;i<numero;i++){
         array_p[i]=(char *) malloc(sizeof(char)*201);
         fscanf(texto,"%s",array_p[i]);
         cambio(array_p[i]);
-	sort(array_p[i],copia)
+	sort(array_p[i],copia);
     }
-    tropical=array_p[0];
+	tropical=array_p[0];
     for(i=0;i<numero;i++){
         printf("%s\n",array_p[i]);
     }
     return array_p;
 }
+int anagramas(char **s,int n, char *q){
+	int i,contador=0;
+	char copia[201];
+	cambio(q);
+	sort(q,copia);
+	for(i=0;i<n;i++){
+		if(strcmp(s[i],q)==0)
+			contador+=1;
+	}
+	return contador;
+}
+		
 int main(){
 	FILE*archivo;
 	archivo=fopen("strings.txt","r");
@@ -63,7 +73,7 @@ int main(){
 		fscanf(archivo,"%d",anagrama);
 		for(i=0;i<anagrama;i++){
 			fscanf(archivo,"%s",leido);
-			anaframas=anagramas(p,numero,leido);
+			anagrama=anagramas(p,numero,leido);
 			fprintf(salida,"%d",anagrama);
 		}
 	free((void *)p);
